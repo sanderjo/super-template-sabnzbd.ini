8169437fa3380bc01d52d5fb76990cd088d20ca8
Sander Jonkers <san.der.jonkers+github@gmail.com>
2026-02-11 23:03:08 +0100

    formatting

--- parse_sabnzbdlog.sh
+++ parse_sabnzbdlog.sh
@@ -3,9 +3,11 @@ echo "All lookups: newsservers with their ipv6:"
 cat logs/sabnzbd.log | grep -i -e Fastest | awk '{ print $6 " " $11 }'  | sort -u | tee all.txt
 
 
+echo
 echo "All failed:"
 cat logs/sabnzbd.log | grep -i -e "no route to host" | awk -F'[@:]' '{print $(NF-1)}'  | sort -u | tee no-route.txt
 
+echo
 echo "FAILED ... with their ipv6 addresses:"
 cat no-route.txt | awk '{ print "grep " $1 " all.txt" }'  | sh
 
